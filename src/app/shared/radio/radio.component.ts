3be472ac9e6bb7ef72f38aaaf433d02f8005a4aa
ggualiato <ggualiato@gmail.com>
2018-03-20 15:20:01 -0300

    Added ControlValueAcessor on RadioComponent

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 
 import { RadioOption } from './radio-option.model';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { fn } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[]
 
   value: any
+
+  onChange: any
 
   constructor() { }
 
@@ -19,6 +30,18 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value
+    this.onChange(this.value)
   }
+
+  writeValue(obj: any): void {
+    this.value = obj
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void { }
+  setDisabledState?(isDisabled: boolean): void { }
 
 }
