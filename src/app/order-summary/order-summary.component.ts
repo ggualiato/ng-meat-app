43b1da7065d7099a58ee29ee9dbafeea7d7ec49b
Giovanni Gualiato <ggualiato@gmail.com>
2018-04-29 01:38:30 -0300

    Wildcards Routes and building app

--- src/app/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSummaryComponent implements OnInit {
 
+  rated: boolean
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  rate() {
+    this.rated = true
+  }
 }
--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -6,5 +6,7 @@ export interface Restaurant {
     deliveryEstimate: string
     rating: number
     imagePath: string
+    hours?: string
+    about?: string
 
 }
