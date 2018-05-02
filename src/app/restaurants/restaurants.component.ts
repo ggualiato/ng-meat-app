ef87e802d5c200425293bd06b2dc03c1ec21eab4
Giovanni Gualiato <ggualiato@gmail.com>
2018-05-01 23:39:05 -0300

    add search by terms

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
+
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from 'rxjs/Observable'
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
@@ -24,19 +32,38 @@ import { RestaurantsService } from './restaurants.service';
 })
 export class RestaurantsComponent implements OnInit {
 
-  searchBarState = 'hidden'
-
+  searchBarState = 'toogleSearch'
   restaurants: Restaurant[]
+  searchForm: FormGroup
+  searchControl: FormControl
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.searchControl = this.fb.control('')
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm)
+        .catch(error => Observable.from([])))
+      .subscribe(restaurants => this.restaurants = restaurants)
+
     this.restaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants)
   }
 
-  toogleSearch() {
+  toogleSearch(inputSearch: HTMLInputElement) {
     this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
+    if (this.searchBarState === 'visible') {
+      inputSearch.focus()
+    }
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -16,8 +16,9 @@ export class RestaurantsService {
 
     constructor(private http: Http) { }
 
-    restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${MEAT_API}/restaurants`)
+    restaurants(search?: string): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`,
+            { params: { q: search } })
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
@@ -36,8 +37,8 @@ export class RestaurantsService {
 
     menuOfRestaurant(id: string): Observable<MenuItem> {
         return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 
 }
